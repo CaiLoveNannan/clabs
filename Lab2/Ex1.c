ecc55d0e488336095c90a7745fb65eceedd4b9ab
LoveNannan <caizhiyuangood@163.com>
2019-11-19 00:10:45 -0800

    added a Ex1.c file

---
+++ Lab2/Ex1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    unsighed    int a   =   1;
+    unsighed    int c   =   0   -   1;
+    int b   =   0;
+    while   (a  !=0){
+        a   =<  1;
+        b   =   b+1;
+    }
+    printf("%d",    b)
+    printf("%u\n",  c)
+    return  0;
+}
